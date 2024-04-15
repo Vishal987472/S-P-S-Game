b3bb7c29e70257a1ff832af6978c993a5d07b359
Vishal987472 <pal7102002@gmail.com>
2024-04-15 22:33:02 +0530

    added

---
+++ index.js
@@ -0,0 +1,60 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const genCompChoice = () => {
+    const options = ["rock","Paper","scissor"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+};
+const drawGame = () => {
+    console.log("Game Was draw");
+    msg.innerText = "Game Was Draw";
+    msg.style.backgroundColor = "#1665b9";
+
+}
+const showWin = (userWin,userChoice, compChoice) => {
+    if(userWin){
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText = `You Win. Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+    }else{
+        console.log("you Loss");
+        compScore++;
+        compScorePara.innerText = compScore;
+        msg.innerText = `You lost. ${compChoice} beats ${userChoice}`;
+        msg.style.backgroundColor = "red";
+    }
+}
+const playgame = (userChoice) => {
+    console.log("user choice =", userChoice);
+    const compChoice = genCompChoice();
+    console.log("comp choice = ",compChoice);
+
+    if(userChoice === compChoice){
+        drawGame();
+    }else {
+        let userWin = true;
+        if(userChoice === "rock"){
+            userWin = compChoice === "Paper" ? false : true;
+        }else if(userChoice === "Paper") {
+            userWin = compChoice === "scissor" ? false : true;
+        }else if(userChoice === "scissor"){
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWin(userWin, userChoice, compChoice);
+    }
+
+};
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playgame(userChoice);
+    });
+})
